f70e0e8bfea27327e4e06ce92b17e28471966207
Kirill Sotnikov <sot-ks@ya.ru>
2022-01-12 21:12:36 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,90 @@
+
+const maindiv = document.querySelector('.main');
+const chColRed = document.querySelector('.choiceColRed');
+const chColGreen = document.querySelector('.choiceColGreen');
+const chColViolet = document.querySelector('.choiceColViolet');
+const sizeplain = 6000;
+let mouseDown = 0;
+document.body.onmousedown = function() { 
+  ++mouseDown;
+  console.log(mouseDown);
+}
+document.body.onmouseup = function() {
+  --mouseDown;
+  console.log(mouseDown);
+}
+for(let j=0;j<sizeplain;j++){
+    const a = document.createElement('button');
+
+    a.classList.add('but');
+
+    maindiv.append(a);
+}
+
+const allbut = document.querySelectorAll('.but');
+// allbut.forEach(item=>{
+//     item.addEventListener('click',()=>{
+//         if(item.classList.contains('bluebut') && mouseDown===1){
+//             item.classList.remove('bluebut');
+//         }else{
+//             if(mouseDown==1){item.classList.add('bluebut')}
+//         }
+//     })
+// });
+
+
+
+chColRed.addEventListener('click',()=>{
+    allbutf(allbut,'redbut');
+})
+
+chColGreen.addEventListener('click',()=>{
+    allbutf(allbut,'greenbut');
+})
+
+chColViolet.addEventListener('click',()=>{
+    allbutf(allbut,'violetbut');
+})
+
+function allbutf(x,y){
+    x.forEach(item=>{
+        item.addEventListener('mouseover',()=>{
+            // if(item.classList.contains(y) && mouseDown==1){
+            //     // item.classList.remove(y);
+            // }else{
+                
+            // }
+            if(item.classList.item(1) && mouseDown==1){
+                item.classList.remove(item.classList.item(1));
+            }
+            if(mouseDown==1){item.classList.add(y)};
+        })
+    });
+}
+
+function showinfo(){
+    alert('1. Выберите цвет. 2. Зажмите левую кнопку мыши и ведите по холсту');
+}
+
+function clearAll(allbutf){
+    allbut.forEach((x)=>{
+        if(x.classList.item(1)!=='but'){
+            x.classList.remove(x.classList.item(1));
+        }
+    })
+}
+
+function lastic(x=allbut,y){
+    x.forEach(item=>{
+        item.addEventListener('mouseover',()=>{
+            // if(item.classList.contains(y) && mouseDown==1){
+            //     // item.classList.remove(y);
+            // }else{
+                
+            // }
+            if(item.classList.item(1) && mouseDown==1){
+                item.classList.remove(item.classList.item(1));
+            }
+        })
+    });
+}
